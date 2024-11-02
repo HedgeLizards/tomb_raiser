75c8a85e2e4681e139e9318130979e3cb9b716e1
troido <troido@protonmail.com>
2024-11-02 18:40:33 +0100

    split select_unit and select_ground

--- code/cursor.gd
+++ code/cursor.gd
@@ -3,25 +3,40 @@ extends Node
 signal selection_changed(selected: Selectable)
 
 
-var selected: Node = null
+# Only one of selected_unit and selected_tile may be non-null at a time
+var selected_unit: Node = null
+var selected_tile: Tile = null
+
 const NOT_CLICKING: Vector2 = Vector2(-1_000_000, -1_000_000)
 var click_start: Vector2 = NOT_CLICKING
 
 func _ready() -> void:
 	print("cursor ready")
 
-func select(unit: Node) -> void:
-	# todo: validate selected
-	self.selected = unit
+func clear_select() -> void:
+	self.selected_unit = null
+	self.selected_tile = null
 	%Selections.clear()
-	if unit != null:
-		print("selected ", unit.mappos, " ", unit)
-		var walkable: Array[Vector2i]
-		walkable.assign(unit.walkable_tiles().keys()) #%Ground.get_surrounding_cells(unit.mappos)
-		for neighbour in walkable:
-			%Selections.set_cell(neighbour, 0, Vector2i.ZERO, 1)
-		#print(walkable)
+	selection_changed.emit(null)
+
+func select_unit(unit: Node) -> void:
+	# todo: validate selected
+	self.selected_unit = unit
+	self.selected_tile = null
+	%Selections.clear()
+	print("selected ", unit.mappos, " ", unit)
+	var walkable: Array[Vector2i]
+	walkable.assign(unit.walkable_tiles().keys()) #%Ground.get_surrounding_cells(unit.mappos)
+	for neighbour in walkable:
+		%Selections.set_cell(neighbour, 0, Vector2i.ZERO, 1)
+	#print(walkable)
 	selection_changed.emit(unit.selectable())
+
+func select_tile(tile: Tile) -> void:
+	self.selected_unit = null
+	self.selected_tile = null
+	%Selections.clear()
+	selection_changed.emit(tile.selectable())
 
 func _unhandled_input(event: InputEvent) -> void:
 	var click_pos: Vector2
@@ -47,13 +62,17 @@ func _unhandled_input(event: InputEvent) -> void:
 	tile_clicked(clicked_tile)
 
 func tile_clicked(pos: Vector2i) -> void:
-	if selected and selected.can_walk_to(pos):
-		selected.walk_to(pos)
-		if selected.can_do_action():
-			select(selected)
+	if selected_unit and selected_unit.can_walk_to(pos):
+		selected_unit.walk_to(pos)
+		if selected_unit.can_do_action():
+			select_unit(selected_unit)
 		else:
-			select(null)
+			clear_select()
 		return
 	var unit: Node2D = %Units.unit_at(pos)
 	if unit != null:
-		select(unit)
+		select_unit(unit)
+		return
+	var tile = %Ground.get_tile(pos)
+	if tile != null:
+		pass
