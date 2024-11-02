a454411b67ab8542ccd40e0121382b710f8f9d0a
troido <troido@protonmail.com>
2024-11-02 19:20:20 +0100

    atlas tiles

--- code/ground.gd
+++ code/ground.gd
@@ -1,14 +1,27 @@
 extends TileMapLayer
 
 
-	
+var WATER: Tile = Tile.new(-1, "Water", "~~~~~")
+var GRASS: Tile = Tile.new(1, "Grass", "An empty field")
+var MOUNTAIN: Tile = Tile.new(2, "Grass", "A tall mountain")
 
 
-func get_tile(pos: Vector2i) -> TileData:
+var tile_index = {
+	Vector2(0, 0): WATER,
+	Vector2(0, 1): GRASS,
+	Vector2(0, 2): MOUNTAIN
+}
+
+func get_tile(pos: Vector2i) -> Tile:
+	if get_cell_source_id(pos) != 1:
+		return null
+	var tile = tile_index.get(get_cell_atlas_coords(pos))
+	print("tile ", tile)
+	return tile_index.get(get_cell_atlas_coords(pos))
 	#var source = tile_set.get_source(get_cell_source_id(pos))
 	##print("source ", source)
 	#var scene = source.get_scene_tile_scene(get_cell_alternative_tile(pos))
 	#print("scene ", scene)
 	#print(scene.walkable)
 	##print("cell  ", get_cell_alternative_tile(pos))
-	return null
+	#return null
--- code/tile.gd
+++ code/tile.gd
@@ -2,10 +2,21 @@ class_name Tile
 extends RefCounted
 
 
-var walkable: bool
-var walk_cost: int
-var description: String
+var walkable: bool = false
+var walk_cost: int = 1
+var description: String = ""
 var title: String
+
+#var tile_source: int = 0
+#var atlas_coord: Vector2i
+
+func _init(walk_cost: int, title: String, description: String) -> void:
+	self.walkable = walk_cost >= 0
+	self.walk_cost = walk_cost
+	self.title = title
+	self.description = description
+	#self.tile_source = tile_source
+	#self.atlas_coord = atlas_coord
 
 func selectable() -> Selectable:
 	var selectable: Selectable = Selectable.new()
@@ -16,3 +27,11 @@ func selectable() -> Selectable:
 		walk_cost: walk_cost
 	}
 	return selectable
+
+#
+#const GRASS = Tile.new()
+#GRASS.walkable = true
+#GRASS.walk_cost = 1
+#GRASS.title = "Grass"
+#GRASS.description = "An empty field"
+#GRASS.atlas_coord = Vector2i(1, 0)
