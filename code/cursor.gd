f9884e5fb79e7a6c1e177077d21d3f9e5d928a99
troido <troido@protonmail.com>
2024-11-02 13:10:39 +0100

    units can walk around

--- code/cursor.gd
+++ code/cursor.gd
@@ -10,16 +10,18 @@ func _ready() -> void:
 func select(unit: Node) -> void:
 	# todo: validate selected
 	self.selected = unit
-	print("selected ", unit)
-	print(unit.mappos)
-	var neighbours: Array[Vector2i] = %Ground.get_surrounding_cells(unit.mappos)
 	%Selections.clear()
-	for neighbour in neighbours:
-		%Selections.set_cell(neighbour, 0, Vector2i.ZERO, 1)
-	print(neighbours)
+	if unit != null:
+		print("selected ", unit.mappos, " ", unit)
+		var neighbours: Array[Vector2i] = %Ground.get_surrounding_cells(unit.mappos)
+		for neighbour in neighbours:
+			%Selections.set_cell(neighbour, 0, Vector2i.ZERO, 1)
+		print(neighbours)
 
 func _unhandled_input(event: InputEvent) -> void:
 	var click_pos: Vector2
+	if event is InputEventMouseMotion:
+		click_start = NOT_CLICKING
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if event.pressed:
 			click_start = event.position
@@ -35,9 +37,17 @@ func _unhandled_input(event: InputEvent) -> void:
 		click_pos = event.position
 	else:
 		return
-	print("mouse ", get_viewport().get_mouse_position())
 	var clicked_tile: Vector2i = %Ground.local_to_map(%Ground.make_canvas_position_local(click_pos))
-	var unit: Node2D = %Units.unit_at(clicked_tile)
-	print("Input!!! " + str(click_pos) + " " + str(clicked_tile) + " " + str(unit))
+	tile_clicked(clicked_tile)
+
+func tile_clicked(pos: Vector2i) -> void:
+	if selected and selected.can_walk_to(pos):
+		selected.walk_to(pos)
+		if selected.can_do_action():
+			select(selected)
+		else:
+			select(null)
+		return
+	var unit: Node2D = %Units.unit_at(pos)
 	if unit != null:
 		select(unit)
--- code/unit.gd
+++ code/unit.gd
@@ -10,3 +10,17 @@ func _ready() -> void:
 	print("unit ready")
 	mappos = tilemap.local_to_map(position)
 	position = tilemap.map_to_local(mappos)
+
+
+
+func can_walk_to(pos: Vector2i) -> bool:
+	return pos in tilemap.get_surrounding_cells(mappos)
+
+func walk_to(pos: Vector2) -> void:
+	mappos = pos
+	# todo: animation
+	position = tilemap.map_to_local(mappos)
+
+func can_do_action() -> bool:
+	# todo: action points
+	return true
