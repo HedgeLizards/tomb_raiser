e4e8dd088ac7901df9383e9e2a6d3e162bf70c9b
troido <troido@protonmail.com>
2024-11-02 11:13:07 +0100

    show neighbour tiles

--- code/cursor.gd
+++ code/cursor.gd
@@ -2,15 +2,21 @@ extends Node
 
 var selected: Node = null
 
+
 func _ready() -> void:
 	print("cursor ready")
 
-func select(selected_unit: Node) -> void:
+func select(unit: Node) -> void:
 	# todo: validate selected
-	self.selected = selected_unit
-	var pos: Vector2i = %Selections.local_to_map(selected_unit.position)
+	self.selected = unit
+	print("selected ", unit)
+	print(unit.mappos)
+	var neighbours: Array[Vector2i] = %Ground.get_surrounding_cells(unit.mappos)
+	%Selections.clear()
+	for neighbour in neighbours:
+		%Selections.set_cell(neighbour, 0, Vector2i.ZERO, 1)
+	print(neighbours)
 
 
 func _on_unit_select_pressed(unit: Node) -> void:
-	print("selected ", unit)
-	#select(unit)
+	select(unit)
--- code/unit.gd
+++ code/unit.gd
@@ -5,10 +5,12 @@ signal select_pressed(unit: Node2D)
 
 @export var tilemap: TileMapLayer
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	position = tilemap.map_to_local(tilemap.local_to_map(position))
+var mappos: Vector2i
 
+
+func _ready() -> void:
+	mappos = tilemap.local_to_map(position)
+	position = tilemap.map_to_local(mappos)
 
 func _on_texture_button_button_up() -> void:
 	print("player clicked")
